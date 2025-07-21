07d53655c5dce80915fa1d841fc4cf4f673ccd78
惠鑫源 <huixinyuan06@126.com>
2025-07-21 10:55:01 -0400

    ch3done

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -95,7 +95,7 @@ pub fn load_apps() {
 }
 
 /// get app info with entry and sp and save `TrapContext` in kernel stack
-pub fn init_app_cx(app_id: usize) -> usize {
+pub fn init_app_cx(app_id: usize) -> usize {//设置trap环境 主要设置每个app的起始执行指令地址和该app的栈指针
     KERNEL_STACK[app_id].push_context(TrapContext::app_init_context(
         get_base_i(app_id),
         USER_STACK[app_id].get_sp(),
--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -14,7 +14,7 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
-use crate::config::MAX_APP_NUM;
+use crate::config::{MAX_APP_NUM,MAX_SYSCALL_NUM};
 use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
 use lazy_static::*;
@@ -54,6 +54,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            syscall_times:[0;  MAX_SYSCALL_NUM]
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -135,7 +136,21 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+    ///increase task's syscall times
+    fn increase_syscall_times(&self,syscall_id: usize) {
+        let mut inner = TASK_MANAGER.inner.exclusive_access();
+        let current=inner.current_task;
+        inner.tasks[current].syscall_times[syscall_id] += 1;
+        drop(inner);
+    }
+    fn get_current_syscall_time(&self,syscall_id:usize)->usize{
+        let  inner = TASK_MANAGER.inner.exclusive_access();
+        let current=inner.current_task;
+        inner.tasks[current].syscall_times[syscall_id]
+    }
+   
 }
+
 
 /// Run the first task in task list.
 pub fn run_first_task() {
@@ -168,4 +183,12 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+///increase task's syscall times
+pub fn increase_syscall_times(syscall_id: usize) {
+    TASK_MANAGER.increase_syscall_times(syscall_id);
+}
+///get current 's syscall times id
+pub fn get_current_syscall_time(syscall_id:usize)->usize{
+    TASK_MANAGER.get_current_syscall_time(syscall_id)
 }
